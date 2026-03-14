51eb4cb7698e13755d5b7875e5877c4d5376f4c6
Xunzhou Ye <yex33@mcmaster.ca>
2026-03-14 15:07:28 -0400

    Remove unused AsRef for SubscriptionStatus for now

--- src/domain/subscription_status.rs
+++ src/domain/subscription_status.rs
@@ -4,12 +4,3 @@ pub enum SubscriptionStatus {
     PendingConfirmation,
     Confirmed,
 }
-
-impl AsRef<str> for SubscriptionStatus {
-    fn as_ref(&self) -> &str {
-        match self {
-            SubscriptionStatus::PendingConfirmation => "pending_confirmation",
-            SubscriptionStatus::Confirmed => "confirmed",
-        }
-    }
-}
