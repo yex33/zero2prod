52c71bfd76bd8f32ad83af0a7472916949578e2f
Xunzhou Ye <yex33@mcmaster.ca>
2026-05-12 20:14:27 -0400

    Add strongly typed session

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,14 +1,15 @@
-use actix_session::Session;
 use actix_web::{HttpResponse, http::header::ContentType, web};
 use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
 
+use crate::session_state::TypedSession;
+
 pub async fn admin_dashboard(
-    session: Session,
+    session: TypedSession,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let username = if let Some(user_id) = session.get::<Uuid>("user_id").map_err(e500)? {
+    let username = if let Some(user_id) = session.get_user_id().map_err(e500)? {
         get_username(user_id, &pool).await.map_err(e500)?
     } else {
         todo!()
--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,4 +1,3 @@
-use actix_session::Session;
 use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
 use actix_web::{HttpResponse, web};
@@ -7,6 +6,7 @@ use secrecy::SecretString;
 use sqlx::PgPool;
 
 use crate::authentication::{AuthError, Credentials, validate_credentials};
+use crate::session_state::TypedSession;
 
 #[derive(thiserror::Error, Debug)]
 pub enum LoginError {
@@ -29,7 +29,7 @@ pub struct FormData {
 pub async fn login(
     web::Form(form): web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.username,
@@ -41,7 +41,7 @@ pub async fn login(
             tracing::Span::current().record("user_id", tracing::field::display(&user_id));
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
